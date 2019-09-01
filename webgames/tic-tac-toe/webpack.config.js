60171034b04bba7aec2839b3a0e55b3a608fe895
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2019-09-01 10:50:37 +0200

    moved as a standalone repo

--- webgames/tic-tac-toe/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-module.exports = {
-    plugins: [
-        new CopyWebpackPlugin([{
-            from: './*.html'
-        }])
-    ]
-}
