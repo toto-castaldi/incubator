2775ee3e40ac659243d9d310a80d3f2b20b8adea
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-10-20 12:15:25 +0200

    first working version insertion_sort

---
+++ introductions-algorithms-thomas-charles-ronald/common.py
@@ -0,0 +1,5 @@
+def read_array(arr, index):
+    return arr[index - 1]
+
+def write_array(arr, index, new_value):
+    arr[index - 1] = new_value
--- introductions-algorithms-thomas-charles-ronald/insertion_sort.py
+++ introductions-algorithms-thomas-charles-ronald/insertion_sort.py
@@ -1,1 +1,16 @@
-print("hello world")
+import numpy
+from common import *
+
+a = numpy.random.randint(1,101,10)
+
+print(a)
+
+for j in range(2, len(a) + 1):
+    key = read_array(a, j)
+    i = j - 1
+    while i > 0 and read_array(a, i) > key:
+        write_array(a, i+1, read_array(a, i))
+        i = i - 1
+    write_array(a, i+1, key)
+
+print(a)
