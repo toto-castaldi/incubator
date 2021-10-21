643dad100725db23bfee8028dd4770a2e11dc400
Toto Antonio Castaldi <toto.castaldi@gmail.com>
2021-10-21 12:02:13 +0200

    insertion gui

--- introductions-algorithms-thomas-charles-ronald/common.py
+++ introductions-algorithms-thomas-charles-ronald/common.py
@@ -1,3 +1,55 @@
+from tkinter import *
+import time
+
+def init_gui():
+    width = 800
+    height = 600
+
+    window = Tk()
+
+    window.title("Insertion sort")
+
+    window.geometry(f"{width}x{height}")
+
+
+    window.rowconfigure(0, weight=1)
+    window.columnconfigure(0, weight=1)
+
+    canvas = Canvas(window)
+    canvas.grid(row=0, column=0, sticky="nsew")
+
+    return window, width, height, canvas
+
+def start_gui(window):
+    window.mainloop()
+
+def print_array(width, height, canvas, arr, sleep_seconds=0.2, red=None ):
+    time.sleep(sleep_seconds)
+
+    canvas.delete("all")
+
+    writeable_height = height * 0.9
+    writeable_width = width * 0.9
+
+    for index, a in enumerate(arr):
+        left_x = writeable_width * 0.05 + index * writeable_width / len(arr) + writeable_width * 0.02
+        bottom_y = writeable_height + writeable_height * 0.05
+        color = "red" if red is not None and index == red else "blue"
+
+        canvas.create_rectangle(
+            left_x,
+            bottom_y,
+            left_x + writeable_width / len(arr) - writeable_width * 0.04,
+            bottom_y - writeable_height * a / 100,
+            fill=color
+        )
+
+    canvas.update()
+
+def write_and_print_array(arr, index, new_value, width, height, canvas, index_red, sleep_seconds=0.2):
+    write_array(arr, index, new_value)
+    print_array(width, height, canvas, arr, sleep_seconds, index_red)
+
 def read_array(arr, index):
     return arr[index - 1]
 
--- introductions-algorithms-thomas-charles-ronald/insertion_sort.py
+++ introductions-algorithms-thomas-charles-ronald/insertion_sort.py
@@ -1,39 +1,24 @@
+from time import sleep
 import numpy
 from common import *
 from tkinter import *
 
-window = Tk()
+window, width, height, canvas = init_gui()
 
-window.title("Insertion sort")
+a = numpy.random.randint(1,101,20)
 
-window.rowconfigure(0, weight=1)
-window.columnconfigure(0, weight=1)
-
-canvas = Canvas(window)
-canvas.grid(row=0, column=0, sticky="nsew")
-
-a = numpy.random.randint(1,101,10)
-
-def print_array(canvas, arr):
-    height = canvas.winfo_reqheight() * 0.9
-    width = canvas.winfo_reqwidth() * 0.9
-
-    for index, a in enumerate(arr):
-        canvas.create_rectangle(width * 0.1  + index * width / len(arr) , height + height * 0.05, width / len(arr) + (index + 1) * width / len(arr) - width * 0.05, height * 0.05 + height * a / 100, fill='blue')
-
-print_array(canvas, a)
+print_array(width, height, canvas, a)
 
 for j in range(2, len(a) + 1):
     key = read_array(a, j)
     i = j - 1
     while i > 0 and read_array(a, i) > key:
-        write_array(a, i+1, read_array(a, i))
-
-        print_array(canvas, a)
+        #a[i+i] = a[i]
+        write_and_print_array(a, i+1, read_array(a, i), width, height, canvas, i, sleep_seconds=0.5)
 
         i = i - 1
-    write_array(a, i+1, key)
 
-print(a)
+    #a[i+1] = key
+    write_and_print_array(a, i+1, key, width, height, canvas, j, sleep_seconds=0.5)
 
-window.mainloop()
+start_gui(window)
