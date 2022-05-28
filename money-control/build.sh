1ab95281c681618a318bd6d19f03d28d87da59b0
Antonio Toto Castaldi <toto.castaldi@gmail.com>
2022-05-28 13:30:09 +0200

    clean

--- money-control/build.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-cd engine
-docker build . -t money-control-engine
-cd ..
-cd postgresql-liquibase
-docker build . -t money-control-liquibase
-cd ..
--- money-control/mobile/app/src/main/java/com/totocastaldi/moneycontrol/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.totocastaldi.moneycontrol.utils
-
-class Constants {
-    companion object {
-        const val BASE_URL = "http://64.227.64.109:5000/"
-    }
-}
